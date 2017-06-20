62d98ac336e376ade2137769ef194c46a552aa5f
heightzhang <2917720939@qq.com>
2017-06-20 09:04:37 +0800

    /*知识点:

--- practice/html1/http.js
+++ practice/html1/http.js
@@ -1,7 +1,18 @@
 var http =require("http");
+//引入 url模块 与 querystring模块
+var url =require('url');
+var querystring =require('querystring');
+
 http.createServer(function(request,response){
-	//设置响应头;
+	//接收传递过来的参数;
+	var paramStr  = url.parse(request.url).query;
+	var param = querystring.parse(paramStr);  
+	console.log('url.parse'+paramStr,'querystring.parse'+param);
+	console.log('改变后'+param["name"]);
+	
+	//设置响应头,解决跨域.
 	response.setHeader("Access-Control-Allow-Origin","*");
+
 	//将相应结果显示浏览器上面
 	response.end("Hello World");
-}).listen(12345);		
+}).listen(12345);
