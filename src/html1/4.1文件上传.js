d502f8c1eca1a0b34be2f04f172f59ced58858a9
heightzhang <2917720939@qq.com>
2017-06-22 17:52:31 +0800

    整理文件

--- src/html1/4.1文件上传.js
+++ src/html1/4.1文件上传.js
@@ -3,23 +3,20 @@ var express = require("express");
 var app = express();
 app.listen(3200);
 
-
 var multer = require('multer');
-/*var upload = multer({
-	//如果用这种方法上传，要手动添加文明名后缀
-	dest: 'uploads/'
-})*/
+
 var storage = multer.diskStorage({
 	//设置上传后文件路径，uploads文件夹需要手动创建。
 	destination: function(req, file, cb) {
 		cb(null, './uploads')
 	},
-	//给上传文件重命名，获取添加后缀名
+	//给上传文件重命名
 	filename: function(req, file, cb) {
 		var fileFormat = (file.originalname).split(".");
-		//给图片加上时间戳格式防止重名名
+		//图片命名规则:name名+时间戳(防止重命名)+后缀名;
+		//注意:file.fieldname为input files html标签的name名;
 		//比如把 abc.jpg图片切割为数组[abc,jpg],然后用数组长度-1来获取后缀名
-		cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+		cb(null, file.fieldname+ '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
 	}
 });
 var upload = multer({
