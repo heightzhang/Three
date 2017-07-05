5bb98400afe5275e1ea53fdd9f9be011ab7ff3c0
heightzhang <2917720939@qq.com>
2017-07-05 16:28:42 +0800

    自定义服务

--- src/html3/拉勾网_angular/controller/listCtrl.js
+++ src/html3/拉勾网_angular/controller/listCtrl.js
@@ -1,6 +1,18 @@
 var app3 = angular.module("app3", []);
 
-app3.controller("listCtrl", function($scope, $http,$rootScope) {
+app3.controller("listCtrl", function($scope, $http, $rootScope) {
+
+    //遮罩层
+    $rootScope.abc = true;
+
+    //查看更多
+    $scope.show_hide = true;
+    $scope.show = function() {
+        $scope.show_hide = false;
+         //正在加载
+        $scope.doing = true;
+    };
+
 
     $http({
         method: "POST",
@@ -15,18 +27,11 @@ app3.controller("listCtrl", function($scope, $http,$rootScope) {
 
         $scope.html = data.data;
 
+        $rootScope.abc = false;
     }, function(err) {
 
     });
 
-    $scope.show_hide = true;
-
-    $rootScope.abc = true;
-
-    $scope.show = function() {
-        $scope.show_hide = false;
-        $scope.doing = true;
-    };
 
     //---------------查看更多-----------------------
     var page = 1;
@@ -51,89 +56,18 @@ app3.controller("listCtrl", function($scope, $http,$rootScope) {
                 $scope.html.push(data.data[i]);
             };
 
-           
+            //正在加载与遮罩层
             $scope.doing = false;
             $rootScope.abc = false;
+
+            //暂无数据与查看更多;
             if (data.data.length < num) {
-                 $scope.last = true;
-                 $scope.show_hide = false;
+                $scope.last = true;
+                $scope.show_hide = false;
             };
 
         }, function(err) {
             console.log(err);
         });
     };
-
-
-
-
-
-
-
-
-
-
-    /* $('.weui-panel__ft .weui-cell__bd').on('click', function() {
-
-          $('.weui-panel__ft').hide();
-          $('.doing').show();
-
-          $('#zhezhao').show();
-
-         page++;
-         var promise = new Promise(function(resolve, reject) {
-             $.ajax({
-                 url: 'http://localhost:8081/',
-                 type: 'get',
-                 data: {
-                     page: page,
-                     num: num
-                 },
-                 succses: function(data) {
-                     resolve(data)
-                 }
-             })
-         });
-         promise.then(function(data) {
-             var html = data.map(function(item) {
-                 return `
-                     <a href="javascript:void(0);" class="weui-media-box weui-media-box_appmsg">
-                     <div class="weui-media-box__hd">
-                         <img class="weui-media-box__thumb" src="http:${item.company_img}" alt="">
-                     </div>
-                     <div class="weui-media-box__bd">
-                         <h4 class="weui-media-box__title">${item.company}</h4>
-                         <p class="weui-media-box__desc">${item.description}</p>
-                     </div>
-                     </a>
-                     `
-             }).join(" ");
-             $(".weui-panel__bd").append(html);
-
-
-
-             // 点击加载更多动画处理;
-             $('.weui-panel__ft').show();
-             $('.doing').hide();
-             //当数据库返回的数字小于7或者为0的时候,暂无数据出现;
-             if (data.length < num) {
-                 $('.weui-panel__ft').hide();
-                 $('.doing').hide();
-                 $('.none').show();
-             }
-             //查看更多后,遮罩层
-             $('#zhezhao').hide();
-
-             //查看更多后,仍然点击页面跳转;
-             $('a.weui-media-box').on('click', function() {
-                 var id = $(this).index() + 1;
-                 location.href = '2.拉勾网详情页.html?id=' + id;
-             })
-
-
-
-
-         });
-
-     });*/
 });
