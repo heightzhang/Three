babf66984fb645f4f378c09b62371531c18773b4
heightzhang <2917720939@qq.com>
2017-07-24 20:35:00 +0800

    更新

---
+++ VUE/html1(webpack搭建)/4.webpack/app/components/xfooter.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+	<div class="weui-tab">
+		<div class="weui-tab__panel">
+		</div>
+		<div class="weui-tabbar">
+			<a href="#!/index/home" class="weui-tabbar__item" ng-class="{'weui-bar__item_on':tab==0}" ng-click="changeTab(0)">
+				<i class="iconfont icon-shouye"></i>
+				<p class="weui-tabbar__label">首页</p>
+			</a>
+			<a href="#!/index/discover" class="weui-tabbar__item" ng-class="{'weui-bar__item_on':tab==1}" ng-click="changeTab(1)">
+				<i class="iconfont icon-faxian"></i>
+				<p class="weui-tabbar__label">发现</p>
+			</a>
+			<a class="weui-tabbar__item" ng-class="{'weui-bar__item_on':tab==3}" ng-click="changeTab(3)">
+				<i class="iconfont icon-fabu"></i>
+				<p class="weui-tabbar__label">发布</p>
+			</a>
+			<a href="#!/index/login" class="weui-tabbar__item" ng-class="{'weui-bar__item_on':tab==4}" ng-click="changeTab(4)">
+				<i class="iconfont icon-wode"></i>
+				<p class="weui-tabbar__label">我</p>
+			</a>
+		</div>
+	</div>
+</div>
+</template>
+<style scoped>
+
+/*脚本*/
+
+.weui-tab {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    z-index: 10;
+    height: 50px;
+}
+</style>
---
+++ VUE/html1(webpack搭建)/4.webpack/app/components/xheader.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<header>
+			{{title}}
+		</header>
+	</div>
+</template>
+<script>
+	export default {
+		computed:{
+			title(){
+				//暴力获取状态
+				return this.$store.state.title
+			}
+		}
+	}
+</script>
+
+<!-- 封闭样式 -->
+<style scoped> 
+	header {
+	    background-color: green;
+	    color: #fff;
+	    height: 50px;
+	    line-height: 50px;
+	    text-align: center;
+	    position: fixed;
+	    width: 100%;
+	    top: 0;
+	    left: 0;
+	    z-index: 10;
+	}
+</style>
